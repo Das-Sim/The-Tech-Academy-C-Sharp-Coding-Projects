4b38a8d71bdd6d05c8ca50176aad26b0be25c91f
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-13 10:37:45 -0500

    Uploade File I/O Example
    As Part of The Tech Academy's C# and .NET Framework Course, Step 154.
    
    
    EXERCISE:
    
    IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
    
    1. Ask a user for a number.
    
    2. Log that number to a text file.
    
    3. Print the text file back to the user.

---
+++ File_IO_Example/File_IO_Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_IO_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.\n"); //1. Ask a user for a number.
+            int num = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\spuns\Documents\blank.txt"))
+            {
+                file.WriteLine(num); //2. Log that number to a text file.
+                file.Close();
+                string file1 = File.ReadAllText(@"C:\Users\spuns\Documents\blank.txt");
+                Console.WriteLine("Your number is " + file1); //3. Print the text file back to the user.
+            }
+            Console.ReadLine();
+        }
+    }
+}
