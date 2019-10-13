df0e7ab6b54bc24971b775539008819b548851eb
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-13 14:52:40 -0500

    Upload Constant/Constructors Pracitce
    As Part of The Tech Academy's C# and .NET Framework Course, Step 162.
    
    EXERCISE:
    
    1. Create a const variable
    
    2. Create a variable using the keyword "var".
    
    3. Chain two constructors together.

---
+++ Constant_Constructors_Practice/Constant_Constructors_Practice/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant_Constructors_Practice
+{
+    public class Employee
+    {
+        public Employee(string name, string city)
+        {
+            const string eName = "Joe Bob"; //1.Create a const variable
+
+            var Company = "Regal's Construction"; //2.Create a variable using the keyword "var".
+
+            Console.WriteLine("\n New Hire: {0} \n Hiring Company: {1}", eName, Company); //3. Chain two constructors together.
+        }
+        public Employee()
+        {
+            
+        }
+    }
+}
---
+++ Constant_Constructors_Practice/Constant_Constructors_Practice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant_Constructors_Practice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            Console.ReadLine();
+        }
+    }
+}
