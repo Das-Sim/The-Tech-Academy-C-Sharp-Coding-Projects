dc60bd0891662d11820a54e0fe0a99d798d8faab
David Simar <50755050+allDasy@users.noreply.github.com>
2019-10-13 11:24:05 -0500

    Upload Chaining Constants Example
    As Part of The Tech Academy's C# and .NET Framework Course, Step 162.
    
    EXERCISE:
    
    1. Create a const variable
    
    2. Create a variable using the keyword "var".
    
    3. Chain two constructors together.

---
+++ Constants_Practice_1/Constants_Practice_1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants_Practice_1
+{
+    class Class1
+    {
+        // 3. Chain two constructors together.
+        public Class1(string skill) : this(50, skill)
+        {
+
+        }
+        public Class1(int points, string name)
+        {
+            Points = points;
+            Class = name;
+           
+        }
+        public int Points { get; set; }
+        public string Class { get; set; }
+    }
+}
---
+++ Constants_Practice_1/Constants_Practice_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants_Practice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Job = "Builder"; //1.Create a const variable
+            Class1 Class = new Class1("Class");
+            var var1 = Class.Points; //2. Create a variable using the keyword "var".
+            Console.WriteLine("Skill Points are {0} for {1}.", var1, Job);
+            Console.Read();
+
+        }       
+    }
+}
